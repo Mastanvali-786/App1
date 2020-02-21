f094f875fa0c92ac26de2514d60a807266547d7a
likhi 444 <Meeravali20eee@gmail.com>
2020-02-21 12:14:29 +0530

    app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-import{ ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-//import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { headerComponent } from './header/header.component';
+
 
 
 
@@ -16,15 +14,13 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
 @NgModule({
   declarations: [
     AppComponent,
-    //WarningAlertComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent
+    headerComponent
   
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    
 
   ],
   providers: [],
