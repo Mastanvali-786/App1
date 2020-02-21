f965edf345cd45de7ef072227a8cbafdcfd1bc55
likhi 444 <Meeravali20eee@gmail.com>
2020-02-21 15:16:13 +0530

    app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { headerComponent } from './header/header.component';
-
-
-
-
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    headerComponent
+    headerComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    
   
   ],
   imports: [
