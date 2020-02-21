45afd1249cea7a423d78d9e8cc2f8ebde39f495b
likhi 444 <Meeravali20eee@gmail.com>
2020-02-21 07:28:35 +0530

    app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
  
  })
 export class AppComponent {
-  
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
 
