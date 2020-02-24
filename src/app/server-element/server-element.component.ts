b53e3b2ae94c096f9785257b4f23b7c9f9cef3de
likhi 444 <Meeravali20eee@gmail.com>
2020-02-25 01:28:06 +0530

    app.component

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -12,7 +12,8 @@ import {
   AfterViewChecked,
   OnDestroy,
   ViewChild,
-  ElementRef
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -33,6 +34,9 @@ OnDestroy{
   @Input('srvElement') element: {type: string, name: string, content:string};
   @Input() name: string;
   @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+
   constructor() { 
     console.log('constructor called!');
   }
@@ -46,6 +50,7 @@ OnDestroy{
   ngOnInit() {
     console.log('ngOnInit called!');
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -54,6 +59,7 @@ OnDestroy{
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked() {
